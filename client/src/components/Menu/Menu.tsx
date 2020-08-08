d3adfaf4f72551f215255ba382b34dba99fce9f6
Lisa <marlisa94@gmx.de>
2020-08-08 21:12:18 +0200

    modify and style settings button

--- client/src/components/Menu/Menu.tsx
+++ client/src/components/Menu/Menu.tsx
@@ -2,28 +2,27 @@ import React from "react";
 import * as styles from "./Menu.module.scss";
 
 export interface MenuProps {
-    unreadCount?: number;
-    toggleSettings: (event: React.MouseEvent<HTMLElement>) => void;
+  unreadCount?: number;
+  toggleSettings: (event: React.MouseEvent<HTMLElement>) => void;
 }
 
 export const Menu: React.FC<MenuProps> = (props) => {
-    return (
-        <div className={styles.menu}>
-            <nav>
-                <ul>
-                    {/*  <li>
-                        <Link to="/">Chat</Link>
-                    </li> */}
-                    <li>
-                        {/* <Link to="/settings">Settings</Link> */}
-                        <button onClick={props.toggleSettings}>Settings</button>
-                    </li>
-                </ul>
-            </nav>
-        </div>
-    );
+  return (
+    <div className={styles.menu}>
+      <nav>
+        <ul>
+          <li>
+            <p>Chat</p>
+          </li>
+          <li>
+            <button onClick={props.toggleSettings}>Settings</button>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 };
 
 Menu.defaultProps = {
-    unreadCount: 0,
+  unreadCount: 0,
 };
--- client/src/components/Settings/SettingsComponent.module.scss.d.ts
+++ client/src/components/Settings/SettingsComponent.module.scss.d.ts
@@ -3,3 +3,4 @@ export const header: string;
 export const settingspart: string;
 export const title: string;
 export const close: string;
+export const closebtn: string;
